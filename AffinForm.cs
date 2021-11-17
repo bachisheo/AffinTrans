d7857a45189a451c3d9973b0436511fed2e0123b
olga <bachisheo@gmail.com>
2021-11-17 19:56:46 +0700

    fix figure list

--- AffinForm.cs
+++ AffinForm.cs
@@ -4,281 +4,239 @@ using System.ComponentModel;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using AF = AffinTransformation.AffinTransformation;
 
 namespace AffinTransformation
 {
     public partial class AffinForm : Form
     {
-        int deltaTrans = 10;
-        int deltaDel = 2;
-        float deltRot = 10;
-        int X0 = 200, Y0 = 400, Z0 = 400;
-        int axesLenght = 350;
-        List<Painter> figurPainters;
-        Painter axesPainter;
-
-        public void AddFigure(Figure figure)
+        private int deltaTrans = 10;
+        private int deltaDel = 2;
+        private float deltRot = 10;
+        private int axesLenght = 350;
+        private const int animTick = 75;
+        private int currentTick;
+        //animated distance
+        private Dot animated;
+        private Dot diff;
+        private int _movableId;
+        private List<Figure> figurs;
+        private Figure _emptyFigure;
+        private Figure _movableFigure
         {
-            figurPainters.Add(new Painter(X0, Y0, Z0, figure));
-
+            set => figurs[_movableId] = value;
+            get => figurs[_movableId];
         }
-        PaintEventArgs paintEventArgs;
+    
+        Figure axes;
+        //speed - delta points in one timer's tik
+        Dot DeltaA = new Dot(0, 0, 0, 1), DeltaB = new Dot(0, 0, 0, 1);
+        bool mbA = false, mbB = false;
+        Dot A = new Dot(0, 0, 0, 1), B = new Dot(0, 0, 0, 1);
         public AffinForm()
         {
-            figurPainters = new List<Painter>();
-            Figure axes = new Figure();
+
+            figurs = new List<Figure>();
+            _emptyFigure = new Figure();
+            _emptyFigure.AddDot(0,0,0);
+            _movableId = 0;
+            figurs.Add(_emptyFigure);
+            axes = new Figure();
             axes.AddDot(0, 0, 0);
             axes.AddDot(axesLenght, 0, 0);
             axes.AddDot(0, axesLenght, 0);
             axes.AddDot(0, 0, axesLenght);
+            axes.AddDot(-15, 100, 0);
+            axes.AddDot(15, 100, 0);
             axes.AddLine(0, 1);
             axes.AddLine(0, 2);
             axes.AddLine(0, 3);
-            axesPainter = new Painter(X0, Y0, Z0, axes);
+            axes.AddLine(4, 5);
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-          
-
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            paintEventArgs = e;
-            axesPainter.Paint(new Pen(Color.Red, 1), e);
-            foreach (Painter paint in figurPainters)
-                paint.Paint(new Pen(Color.Black, 1), e);
-
+            AF.Paint(axes, new Pen(Color.Red, 1), e);
+            foreach(var fig in figurs)
+                AF.Paint(fig, new Pen(fig.Color, 1), e);
+                AF.Paint(_movableFigure, new Pen(_movableFigure.Color, 1), e);
         }
-
-        private void buttonXAdd_Click(object sender, EventArgs e)
+        private void Transfer_Click(object sender, EventArgs e)
         {
-            foreach (Painter fp in figurPainters)
+            Button but = (Button)sender;
+            switch (but.Text)
             {
-                fp.Translation(deltaTrans, 0, 0);
+                case "+X": _movableFigure = AF.Translation(_movableFigure, deltaTrans, 0, 0); break;
+                case "+Y": _movableFigure = AF.Translation(_movableFigure, 0, deltaTrans, 0); break;
+                case "+Z": _movableFigure = AF.Translation(_movableFigure, 0, 0, deltaTrans); break;
+                case "--X": _movableFigure = AF.Translation(_movableFigure, -deltaTrans, 0, 0); break;
+                case "--Y": _movableFigure = AF.Translation(_movableFigure, 0, -deltaTrans, 0); break;
+                case "--Z": _movableFigure = AF.Translation(_movableFigure, 0, 0, -deltaTrans); break;
             }
             Refresh();
         }
-
-        private void buttonZAdd_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Translation(0, 0, deltaTrans);
-            }
-            Refresh();
-        }
-
-        private void buttonZSubs_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Translation(0, 0, -deltaTrans);
-            }
-            Refresh();
-        }
-
-        private void buttonXSub_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Translation(-deltaTrans, 0, 0);
-            }
-            Refresh();
-        }
-
-        private void buttonYSubs_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Translation(0, -deltaTrans, 0);
-            }
-            Refresh();
-        }
-
+       
         private void buttonReset_Click(object sender, EventArgs e)
         {
-            foreach (Painter fp in figurPainters)
+            _movableFigure.Reset();
+            Refresh();
+        }
+
+        
+        private void Dialatation_Click(object sender, EventArgs e)
+        {
+            Button but = (Button)sender;
+            switch (but.Text)
             {
-                fp.ResetTransformation();
+                case "X": _movableFigure = AF.Dilatation(_movableFigure, deltaDel, 1, 1); break;
+                case "Y": _movableFigure = AF.Dilatation(_movableFigure, 1, deltaDel, 1); break;
+                case "Z": _movableFigure = AF.Dilatation(_movableFigure, 1, 1, deltaDel); break;
+                case "x": _movableFigure = AF.Dilatation(_movableFigure, 1.0f / deltaDel, 1, 1); break;
+                case "y": _movableFigure = AF.Dilatation(_movableFigure, 1, 1.0f / deltaDel, 1); break;
+                case "z": _movableFigure = AF.Dilatation(_movableFigure, 1, 1, 1.0f / deltaDel); break;
+            }
+            Refresh();
+        } 
+        private void Rotation_Click(object sender, EventArgs e)
+        {
+            Button but = (Button)sender;
+            switch (but.Text)
+            {
+                case "-Y": _movableFigure = AF.Rotation(_movableFigure, 'y', -deltRot); break;
+                case "+Y": _movableFigure = AF.Rotation(_movableFigure, 'y', deltRot); break;
+                case "-X": _movableFigure = AF.Rotation(_movableFigure, 'x', -deltRot); break;
+                case "+X": _movableFigure = AF.Rotation(_movableFigure, 'x', deltRot); break;
+                case "-Z": _movableFigure = AF.Rotation(_movableFigure, 'z', -deltRot); break;
+                case "+Z": _movableFigure = AF.Rotation(_movableFigure, 'z', deltRot); break;
             }
             Refresh();
         }
 
-        private void AffinKeyPress(object sender, KeyPressEventArgs e)
+        private void RefButton_Click(object sender, EventArgs e)
         {
-            Painter fp = figurPainters[0];
-
-            switch (e.KeyChar)
+            Button but = (Button)sender;
+            switch (but.Text)
             {
-                case 'A': fp.Translation(-deltaTrans, 0, 0); break;
-                case 'F': fp.Translation(deltaTrans, 0, 0); break;
-                case 'E': fp.Translation(0, deltaTrans, 0); break;
-                case 'X': fp.Translation(0, -deltaTrans, 0); break;
-                case 'S': fp.Translation(0, 0, deltaTrans); break;
-                case 'D': fp.Translation(0, 0, -deltaTrans); break;
-            }
-        }
-
-        private void buttonYAdd_Click(object sender, MouseEventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Translation(0, deltaTrans, 0);
+                case "Z": _movableFigure = AF.Reflection(_movableFigure, false, false, true); break;
+                case "X": _movableFigure = AF.Reflection(_movableFigure, true, false, false); break;
+                case "Y": _movableFigure = AF.Reflection(_movableFigure, false, true, false); break;
             }
             Refresh();
         }
 
-        private void buttonDurZAdd_Click(object sender, EventArgs e)
+    
+        private void MoveByLine_Click(object sender, EventArgs e)
         {
-            foreach (Painter fp in figurPainters)
+            Dot div = new Dot(AF.x0, AF.y0, 0, 0);
+            A.x = labA.Left - (int)AF.x0;
+            A.y = -(labA.Top - (int)AF.y0);
+            B.x = labB.Left - (int)AF.x0;
+            B.y = -(labB.Top - (int)AF.y0);
+
+            animated = new Dot(0,0,0,1);
+            _movableFigure = AF.Translation(_movableFigure,  _movableFigure.GetBaseDot(0) - _movableFigure.GetDot(0));
+      
+            _movableFigure = AF.Translation(_movableFigure, A);
+            diff = B - A;
+            currentTick = 0;
+            this.timer1.Start();
+           Refresh();
+        }
+
+        float Progress(int curTick)
+        {
+            float x = (1f * curTick) / animTick;
+            return -(x - 1)*(x - 1) + 1; 
+        }
+        Dot CurrentDelta(float progr)
+        {
+            Dot cur = progr * diff;
+            Dot delta = cur - animated;
+            animated = cur;
+            return delta;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(++currentTick == animTick)
             {
-                fp.Dilatation(1, 1, deltaDel);
+                timer1.Stop();
             }
+            _movableFigure = AF.Translation(_movableFigure, CurrentDelta(Progress(currentTick)));
             Refresh();
         }
 
-        private void buttonDurZDel_Click(object sender, EventArgs e)
-        {
-
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Dilatation(1,1, 1.0f/deltaDel);
-            }
-            Refresh();
-        }
-
-        private void DilatIncX_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Dilatation(deltaDel, 1, 1);
-            }
-            Refresh();
-        }
-
-        private void DilatDecrX_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Dilatation(1.0f/deltaDel, 1, 1);
-            }
-            Refresh();
-        }
-
-        private void DilatIncY_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Dilatation(1, deltaDel, 1);
-            }
-            Refresh();
-        }
-
-        private void DilatDecrY_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Dilatation(1, 1.0f / deltaDel, 1);
-            }
-            Refresh();
-        }
-
-        private void RotZPlus_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationZ(deltRot);
-            }
-            Refresh();
-        }
-
-        private void RotXPlus_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationX(deltRot);
-            }
-            Refresh();
-        }
-
-        private void RotXNeg_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationX(-deltRot);
-            }
-            Refresh();
-        }
-
-        private void RotZNeg(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationZ(-deltRot);
-            }
-            Refresh();
-        }
-
-        private void RotYPos_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationY(deltRot);
-            }
-            Refresh();
-        }
-
-        private void RotYNeg_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.RotationY(-deltRot);
-            }
-            Refresh();
-
-        }
-
-        private void RefXButton_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Reflection(true, false, false);
-            }
-            Refresh();
-        }
-
-        private void RefYButton_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Reflection(false, true, false);
-            }
-            Refresh();
-        }
-
-        private void RefZButton_Click(object sender, EventArgs e)
-        {
-            foreach (Painter fp in figurPainters)
-            {
-                fp.Reflection(false, false, true);
-            }
-            Refresh();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void AddNewFigure(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 TextReader tr = File.OpenText(openFileDialog1.FileName);
-                Figure f = new Figure();
-                f.Input(tr);
+
+                figurs.Add(new Figure());
+                _movableId = figurs.Count - 1;
+                _movableFigure.Input(tr);
                 tr.Close();
-                AddFigure(f);
                 this.Refresh();
             }
+        }
+
+        private void labA_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) //Если нажата левая кнопка мыши
+            {
+                mbA = true; //Запоминаем статус нажатости
+                DeltaA.x = Cursor.Position.X - labA.Location.X; //Запоминаем значения дельта
+                DeltaA.y = Cursor.Position.Y - labA.Location.Y;
+            }
+
+        }
+
+        private void labB_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Left) //Если нажата левая кнопка мыши
+            {
+                mbB = true; //Запоминаем статус нажатости
+                DeltaB.x = Cursor.Position.X - labB.Location.X; //Запоминаем значения дельта
+                DeltaB.y = Cursor.Position.Y - labB.Location.Y;
+            }
+        }
+
+        private void labB_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mbB) //Если нажата и удерживается левая кнопка мыши
+            {
+                labB.Left = Cursor.Position.X - (int)DeltaB.x; //устанавливаем лейбл в новом месте относительно нового положения курсора экрана
+                labB.Top = Cursor.Position.Y - (int)DeltaB.y;
+            }
+        }
+
+        private void labB_MouseUp(object sender, MouseEventArgs e)
+        {
+            mbB = false;
+            DeltaB.x = 0;
+            DeltaB.y = 0;
+        }
+
+        private void labA_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (mbA) //Если нажата и удерживается левая кнопка мыши
+            {
+                labA.Left = Cursor.Position.X - (int)DeltaA.x; //устанавливаем лейбл в новом месте относительно нового положения курсора экрана
+                labA.Top = Cursor.Position.Y - (int)DeltaA.y;
+            }
+        }
+
+        private void labA_MouseUp(object sender, MouseEventArgs e)
+        {
+            mbA = false;
+            DeltaA.x = 0;
+            DeltaA.y = 0;
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
--- Figure.cs
+++ Figure.cs
@@ -6,30 +6,7 @@ using System.Windows.Forms;
 namespace AffinTransformation
 {
 
-    public struct Dot
-    {
-        public float x, y, z, h;
-        public Dot(float x, float y, float z, float h)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.h = h;
-        }
-        public Dot Product(float[,] matrix)
-        {
-            float[] coord = { 0, 0, 0, 0 };
-            float[] old = { x, y, z, h };
-            for (int j = 0; j < 4; j++)
-            {
-                for (int k = 0; k < 4; k++)
-                {
-                    coord[j] += old[k] * matrix[k, j];
-                }
-            }
-            return new Dot(coord[0], coord[1], coord[2], coord[3]);
-        }
-    }
+
     public struct Line
     {
         public int aNum, bNum;
@@ -38,15 +15,17 @@ namespace AffinTransformation
             aNum = a;
             bNum = b;
         }
-        
     }
-   public class Figure
+    public class Figure
     {
+        public Color Color { get; set; }
         public const int DEM = 4;
         public Figure()
         {
+            baseDots = new List<Dot>();
             dots = new List<Dot>();
             lines = new List<Line>();
+            Color = Color.BlueViolet;
         }
         public void Input(in TextReader tr)
         {
@@ -56,6 +35,7 @@ namespace AffinTransformation
             dotCount = int.Parse(text[ind++]);
             lineCount = int.Parse(text[ind++]);
             dots = new List<Dot>(dotCount);
+            baseDots = new List<Dot>(dotCount);
             lines = new List<Line>(lineCount);
             for (int i = 0; i < dotCount; i++)
             {
@@ -77,36 +57,34 @@ namespace AffinTransformation
         public void AddDot(float x, float y, float z)
         {
             dots.Add(new Dot(x, y, z, 1));
+            baseDots.Add(new Dot(x, y, z, 1));
         }
-        public void AddLine(int a, int b)
-        {
-            lines.Add(new Line(a, b));
-        }
-        public List<Line> GetLines()
-        {
-            return lines;
-        }
-        public Dot GetDot(int index)
-        {
-            return dots[index];
-        }
-      
+        public void AddLine(int a, int b) => lines.Add(new Line(a, b));
+        public List<Line> GetLines() => lines;
+        public Dot GetDot(int index) => dots[index];
+        public Dot GetBaseDot(int index) => baseDots[index];
+
+
         public Figure Product(float[,] matrix)
         {
             Figure prod = new Figure();
             prod.lineCount = lineCount;
             prod.dotCount = dotCount;
+            prod.baseDots = baseDots;
             prod.lines = new List<Line>(lines);
             prod.dots = new List<Dot>(dotCount);
             foreach (Dot dot in dots)
             {
-                prod.dots.Add(dot.Product(matrix));
+                prod.dots.Add(dot * matrix);
             }
             return prod;
         }
+        public void Reset() => dots = new List<Dot>(baseDots);
+
         int dotCount;
         int lineCount;
         List<Dot> dots;
+        List<Dot> baseDots;
         List<Line> lines;
 
     }
